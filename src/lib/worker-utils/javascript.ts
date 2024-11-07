d923dd5c9dbed5249d3367d07b92b3d2921e3b34
jdrydn <james@jdrydn.com>
2024-11-07 22:42:49 +0000

    fix: Transforming stack traces

---
+++ src/lib/worker-utils/javascript.ts
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-useless-escape
+const ERR_REGEX = /eval at <anonymous> \(eval at makeEvaluate \(([^\)]+)\)\), /g;
+
+export function cleanErrorStack(filename: string, stack: string[]) {
+  return stack.reduce((list, line) => {
+    if (ERR_REGEX.test(line)) {
+      const transformed = line
+        .replace(ERR_REGEX, '')
+        .replace('<anonymous>:', `${filename}:`)
+        .replace(/Object\.eval\s*/, '');
+
+      list.push(transformed);
+    }
+
+    return list;
+  }, [] as string[]);
+}
